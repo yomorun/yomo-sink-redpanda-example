bbf64e3d69e6789c72f137f2b0d8fd6cef1a6317
Hong Xiaojian <xiaojian.hong@gmail.com>
2021-04-26 18:11:33 +0800

    Add more comments

--- app.go
+++ app.go
@@ -10,28 +10,29 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/reactivex/rxgo/v2"
 	y3 "github.com/yomorun/y3-codec-golang"
 	"github.com/yomorun/yomo/pkg/rx"
 )
 
-// noiseDataKey represents the Tag of a Y3 encoded data packet
+// noiseDataKey represents the Tag of a Y3 encoded data packet.
 const noiseDataKey = 0x10
-const batchSize = 1000
-const bufferSeconds = 30
+
+// batchSize is the amount of data that will be inserted into Redpanda in batch.
+const batchSize = 100
+
+// bufferMilliseconds is the time in milliseconds that the data will be buffered and inserted into Redpanda in batch.
+const bufferMilliseconds = 3e3
 
 var (
-	pandaproxyURL = ""
-	topic         = ""
-	bufferTime    = rxgo.WithDuration(bufferSeconds * time.Second)
+	pandaProxyURL = "" // Redpanda Proxy URL.
+	topic         = "" // Topic name.
 )
 
 func init() {
-	pandaproxyURL = os.Getenv("PANDAPROXY_URL")
-	if pandaproxyURL == "" {
-		pandaproxyURL = "http://localhost:8082"
+	pandaProxyURL = os.Getenv("PANDAPROXY_URL")
+	if pandaProxyURL == "" {
+		pandaProxyURL = "http://localhost:8082"
 	}
 	topic = os.Getenv("REDPANDA_TOPIC")
 	if topic == "" {
@@ -39,26 +40,29 @@ func init() {
 	}
 }
 
+// noiseData represents the structure of data.
 type noiseData struct {
 	Noise float32 `y3:"0x11" json:"noise"`
 	Time  int64   `y3:"0x12" json:"time"`
 	From  string  `y3:"0x13" json:"from"`
 }
 
+// postData represents the structure of records that will be posted to Redpanda.
 type postData struct {
-	Records []noiseDataItem `json:"records"`
+	Records []postDataItem `json:"records"`
 }
 
-type noiseDataItem struct {
+// postDataItem represents the structure of data item.
+type postDataItem struct {
 	Value     interface{} `json:"value"`
 	Partition int         `json:"partition"`
 }
 
-// get POST body to Redpanda Proxy
+// getPostBody gets the body of HTTP POST to Redpanda Proxy.
 func getPostBody(data []interface{}) ([]byte, error) {
-	items := make([]noiseDataItem, len((data)))
+	items := make([]postDataItem, len((data)))
 	for i, noise := range data {
-		items[i] = noiseDataItem{
+		items[i] = postDataItem{
 			Value:     noise,
 			Partition: 0,
 		}
@@ -68,7 +72,7 @@ func getPostBody(data []interface{}) ([]byte, error) {
 	})
 }
 
-// write to Redpanda
+// write data to Redpanda Proxy in batch via RESTful API.
 var produce = func(_ context.Context, v interface{}) (interface{}, error) {
 	data, ok := v.([]interface{})
 	if !ok {
@@ -80,7 +84,7 @@ var produce = func(_ context.Context, v interface{}) (interface{}, error) {
 	}
 
 	// post data to Redpanda
-	resp, err := http.Post(fmt.Sprintf("%s/topics/%s", pandaproxyURL, topic), "application/vnd.kafka.binary.v2+json", bytes.NewBuffer(postBody))
+	resp, err := http.Post(fmt.Sprintf("%s/topics/%s", pandaProxyURL, topic), "application/vnd.kafka.binary.v2+json", bytes.NewBuffer(postBody))
 	if err != nil {
 		log.Fatalln(err)
 		return nil, err
@@ -92,7 +96,7 @@ var produce = func(_ context.Context, v interface{}) (interface{}, error) {
 		log.Fatalln(err)
 		return nil, err
 	}
-
+	// print the response
 	log.Printf(string(body))
 
 	return fmt.Sprintf("⚡️ write %d items to redpanda successfully", len(data)), nil
@@ -101,12 +105,14 @@ var produce = func(_ context.Context, v interface{}) (interface{}, error) {
 // y3 callback
 var callback = func(v []byte) (interface{}, error) {
 	var mold noiseData
+	// decode bytes by Y3 Codec.
 	err := y3.ToObject(v, &mold)
 
 	if err != nil {
 		return nil, err
 	}
 
+	// return the JSON encoding for insertion in Redpanda.
 	b, err := json.Marshal(mold)
 	if err != nil {
 		return nil, err
@@ -119,7 +125,7 @@ func Handler(rxstream rx.RxStream) rx.RxStream {
 	stream := rxstream.
 		Subscribe(noiseDataKey).
 		OnObserve(callback).
-		BufferWithTimeOrCount(bufferTime, batchSize).
+		BufferWithTimeOrCount(bufferMilliseconds, batchSize).
 		Map(produce).
 		StdOut().
 		Encode(noiseDataKey)
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/yomorun/yomo-sink-redpanda-example
 go 1.15
 
 require (
-	github.com/reactivex/rxgo/v2 v2.4.0
-	github.com/yomorun/y3-codec-golang v1.6.6
+	github.com/yomorun/y3-codec-golang v1.6.8
 	github.com/yomorun/yomo v1.0.0
 )
